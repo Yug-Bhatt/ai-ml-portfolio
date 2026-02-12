4e399e621c6633ef5cc1263b6f02420c73b76cb6
Yug-Bhatt <24aiml003@charusat.edu.in>
2026-02-12 21:45:15 +0530

    particles

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -1,33 +1,34 @@
 "use client";
 
 import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import { loadSlim } from "tsparticles-slim";
 
 export default function ParticlesBackground() {
-  const particlesInit = async (main: any) => {
-    await loadFull(main);
+  const particlesInit = async (engine: any) => {
+    await loadSlim(engine);
   };
 
   return (
     <Particles
-      id="tsparticles"
       init={particlesInit}
       options={{
+        fullScreen: { enable: false },
         background: {
           color: "transparent",
         },
         particles: {
           number: {
-            value: 60,
+            value: 80,
           },
           color: {
-            value: "#60a5fa",
+            value: "#3b82f6",
           },
           links: {
             enable: true,
-            color: "#60a5fa",
-            distance: 120,
+            color: "#3b82f6",
+            distance: 140,
             opacity: 0.4,
+            width: 1,
           },
           move: {
             enable: true,
